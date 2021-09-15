4f1a0f9f0d58a7b49afc96dfa35abcd176dd0eb6
Baotong LU <baotonglu@Baotongs-MacBook-Pro.local>
2021-09-16 00:40:03 +0800

    first clear the repo

--- total.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#APEX evaluation
-./full.sh longitudes-200M.bin.data binary double 100000000 200000000 full 1 alex 1 0 > apex-longitudes-full.data
-./full.sh longitudes-200M.bin.data binary double 100000000 200000000 insert 1 alex 1 0 > apex-longitudes-insert.data
-./full.sh longitudes-200M.bin.data binary double 90000000 200000000 erase 1 alex 1 0 > apex-longitudes-erase.data
