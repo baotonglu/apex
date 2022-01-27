2ae0e5290e87f9cdae4b44173ddd74074f425225
Baotong LU <lubaotong22@gmail.com>
2022-01-27 18:00:51 +0800

    further debug

--- src/core/apex.h
+++ src/core/apex.h
@@ -1746,6 +1746,12 @@ public:
       ADD(&stats_.num_keys, (1 << 19));
     }
 
+    if (superroot_->num_children_ != 1) {
+      printf("Insert key = %.10f\n", key);
+      std::cout << "lock value = " << superroot_->num_children_ << std::endl;
+      exit(-1);
+    }
+
     if (superroot_->lock_ != 0) {
       printf("Insert key = %.10f\n", key);
       std::cout << "lock value = " << superroot_->lock_ << std::endl;
